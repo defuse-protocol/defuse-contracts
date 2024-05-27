4889595eb24119465af46cad6b2d782aa30f4819
Alex Gonchar <168727861+gonchar1987@users.noreply.github.com>
2024-05-27 09:38:22 +0100

    fix: check time and block expiration in an intent (#1)

--- tests/src/tests/intent/mod.rs
+++ tests/src/tests/intent/mod.rs
@@ -197,3 +197,109 @@ async fn test_rollback_intent() {
     assert_eq!(env.token_b.ft_balance_of(env.user_id()).await, 0);
     assert_eq!(env.token_b.ft_balance_of(env.solver_id()).await, 2000);
 }
+
+#[tokio::test]
+async fn test_block_expired_intent() {
+    test_expired_intent(Expiration::Block(1), Expiration::Block(1000)).await;
+}
+
+#[tokio::test]
+async fn test_time_expired_intent() {
+    let now = std::time::SystemTime::now()
+        .duration_since(std::time::UNIX_EPOCH)
+        .unwrap()
+        .as_secs();
+
+    test_expired_intent(Expiration::Time(now - 120), Expiration::Time(now + 120)).await;
+}
+
+async fn test_expired_intent(past: Expiration, future: Expiration) {
+    let env = Env::create().await;
+
+    // Deposit 1000 TokenA to the user and 2000 TokenB to the solver.
+    env.token_a.ft_transfer(env.user_id(), 1000).await;
+    env.token_b.ft_transfer(env.solver_id(), 2000).await;
+
+    // Check that the user doesn't have TokenB and the solver TokenA.
+    assert_eq!(env.token_a.ft_balance_of(env.user_id()).await, 1000);
+    assert_eq!(env.token_b.ft_balance_of(env.user_id()).await, 0);
+
+    assert_eq!(env.token_a.ft_balance_of(env.solver_id()).await, 0);
+    assert_eq!(env.token_b.ft_balance_of(env.solver_id()).await, 2000);
+
+    // User creates an intent which is already expired and shouldn't be executed.
+    env.user
+        .create_intent(
+            env.token_a.id(),
+            env.intent.id(),
+            "1",
+            types::Intent {
+                initiator: env.user_id().clone(),
+                send: TokenAmount {
+                    token_id: env.token_a.id().clone(),
+                    amount: 1000.into(),
+                },
+                receive: TokenAmount {
+                    token_id: env.token_b.id().clone(),
+                    amount: 2000.into(),
+                },
+                expiration: past,
+            },
+        )
+        .await;
+
+    // Check that intent contract owns user's TokenA now.
+    assert_eq!(env.token_a.ft_balance_of(env.user_id()).await, 0);
+    assert_eq!(env.token_a.ft_balance_of(env.intent.id()).await, 1000);
+
+    let intent = env.user.get_intent(env.intent.id(), "1").await;
+    assert!(intent.is_some());
+
+    // The solver is happy with such intent and executes it.
+    env.solver
+        .execute_intent(env.token_b.id(), env.intent.id(), "1", 2000.into())
+        .await;
+
+    let intent = env.user.get_intent(env.intent.id(), "1").await;
+    assert!(intent.is_none());
+
+    // Check balances after intent execution.
+    assert_eq!(env.token_a.ft_balance_of(env.user_id()).await, 1000);
+    assert_eq!(env.token_a.ft_balance_of(env.solver_id()).await, 0);
+
+    assert_eq!(env.token_b.ft_balance_of(env.user_id()).await, 0);
+    assert_eq!(env.token_b.ft_balance_of(env.solver_id()).await, 2000);
+
+    // User creates an intent which will be expired in the future and should be executed.
+    env.user
+        .create_intent(
+            env.token_a.id(),
+            env.intent.id(),
+            "2",
+            types::Intent {
+                initiator: env.user_id().clone(),
+                send: TokenAmount {
+                    token_id: env.token_a.id().clone(),
+                    amount: 1000.into(),
+                },
+                receive: TokenAmount {
+                    token_id: env.token_b.id().clone(),
+                    amount: 2000.into(),
+                },
+                expiration: future,
+            },
+        )
+        .await;
+
+    // The solver is happy with such intent and executes it.
+    env.solver
+        .execute_intent(env.token_b.id(), env.intent.id(), "2", 2000.into())
+        .await;
+
+    // Check balances after intent execution.
+    assert_eq!(env.token_a.ft_balance_of(env.user_id()).await, 0);
+    assert_eq!(env.token_b.ft_balance_of(env.user_id()).await, 2000);
+
+    assert_eq!(env.token_a.ft_balance_of(env.solver_id()).await, 1000);
+    assert_eq!(env.token_b.ft_balance_of(env.solver_id()).await, 0);
+}
