e671d0c133056fc8ba44e98f09f85231e7bcddc6
De Fuse <fuzdefuz@gmail.com>
2025-01-31 19:00:47 +0400

    test: upgrade

--- tests/src/tests/defuse/accounts.rs
+++ tests/src/tests/defuse/accounts.rs
@@ -8,6 +8,19 @@ pub trait AccountManagerExt {
         defuse_contract_id: &AccountId,
         public_key: PublicKey,
     ) -> anyhow::Result<()>;
+
+    async fn defuse_has_public_key(
+        &self,
+        defuse_contract_id: &AccountId,
+        account_id: &AccountId,
+        public_key: &PublicKey,
+    ) -> anyhow::Result<bool>;
+
+    async fn has_public_key(
+        &self,
+        account_id: &AccountId,
+        public_key: &PublicKey,
+    ) -> anyhow::Result<bool>;
 }
 
 impl AccountManagerExt for near_workspaces::Account {
@@ -28,6 +41,31 @@ impl AccountManagerExt for near_workspaces::Account {
             .into_result()?;
         Ok(())
     }
+
+    async fn defuse_has_public_key(
+        &self,
+        defuse_contract_id: &AccountId,
+        account_id: &AccountId,
+        public_key: &PublicKey,
+    ) -> anyhow::Result<bool> {
+        self.view(defuse_contract_id, "has_public_key")
+            .args_json(json!({
+                "account_id": account_id,
+                "public_key": public_key,
+            }))
+            .await?
+            .json()
+            .map_err(Into::into)
+    }
+
+    async fn has_public_key(
+        &self,
+        account_id: &AccountId,
+        public_key: &PublicKey,
+    ) -> anyhow::Result<bool> {
+        self.defuse_has_public_key(self.id(), account_id, public_key)
+            .await
+    }
 }
 
 impl AccountManagerExt for near_workspaces::Contract {
@@ -38,6 +76,27 @@ impl AccountManagerExt for near_workspaces::Contract {
     ) -> anyhow::Result<()> {
         self.as_account()
             .add_public_key(defuse_contract_id, public_key)
+            .await
+    }
+
+    async fn defuse_has_public_key(
+        &self,
+        defuse_contract_id: &AccountId,
+        account_id: &AccountId,
+        public_key: &PublicKey,
+    ) -> anyhow::Result<bool> {
+        self.as_account()
+            .defuse_has_public_key(defuse_contract_id, account_id, public_key)
+            .await
+    }
+
+    async fn has_public_key(
+        &self,
+        account_id: &AccountId,
+        public_key: &PublicKey,
+    ) -> anyhow::Result<bool> {
+        self.as_account()
+            .has_public_key(account_id, public_key)
             .await
     }
 }
